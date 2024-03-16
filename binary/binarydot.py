bb87f2b815ac96fde1f08f940f8eb91262bd9efa
Shruti Karthikeyan <160132808+Shrulex@users.noreply.github.com>
2024-03-16 18:27:16 +0530

    test

---
+++ binary/binarydot.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = generate_password_hash(password)
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('Unknown email address')
+
+class SignupForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already taken.')
+
+def login_required(view):
+    def wrapped_view(**kwargs):
+        if not session.get('logged_in'):
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+        return view(**kwargs)
+    return wrapped_view
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            session['logged_in'] = True
+            flash('Login successful.')
+            return redirect(url_for('homepage'))  # Corrected route to 'homepage'
+        else:
+            flash('Login failed. Please check your email and password.')
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(form.email.data, form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Signup successful.')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+@app.route('/homepage')
+def homepage():
+    if not session.get('logged_in'):
+        flash('You need to login first.')
+        return redirect(url_for('login'))
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
